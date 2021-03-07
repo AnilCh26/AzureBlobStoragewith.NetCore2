cfec978e9fc578ebeb87f2e755a73f7aae4a5043
Anil Chintala <anil.chintala@minterellison.co.nz>
2021-03-07 15:24:55 +1300

    Add project files.

---
+++ DataContext/DataContext/EFDBContext.cs
@@ -0,0 +1,32 @@
+﻿using DataContext.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataContext.DataContext
+{
+   public class EFDBContext :DbContext
+    {
+        public EFDBContext()
+        {
+
+        }
+
+        public EFDBContext(DbContextOptions<EFDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"data source=Deb; initial catalog=Pers_ProjectDB;persist security info=True;user id=sa;password=abc123;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
